312ea2e175b262fc4ac7206906054f745514d3a7
Michael Bradley <mbrad94@gmail.com>
2017-02-13 08:38:04 -0800

    Final Changes
    - added more documentation
    - added convenience methods
    - added copy method and constructor
    - changed getInput(boolean) to use isValidOption(int) for input validation
    - changed mutators for exit, chosen, and unchosen texts to accept null arguments
    - changed setComplete(boolean) to return a reference to its menu object
    - changed buildFromFile(String) to loadFromFile(String[])

--- src/com/github/HotPocketAdventure/util/Menu.java
+++ src/com/github/HotPocketAdventure/util/Menu.java
@@ -19,11 +19,9 @@ import com.github.HotPocketAdventure.util.exceptions.InvalidMemberException;
 
 /**
  * A basic text menu utility that uses the standard input/output for displaying and retrieving the user's choices.
- * <br>The menu can keep track of which menu options have and have not yet been selected.  
- * <br>All data members must have values that follow the rules of the {@link #validateMembers() validateMembers} method.
- * <br>A null argument can be passed to the options array to allocate an empty array of size numOptions.
- * <br>A null argument can be passed to the title to make it an empty string.
- * <br>When resetting the options, numOptions or options array may be changed in any order, but the {@link #initChosen() initChosen} method must be called after setting numOptions.
+ * <br>- The menu can keep track of which menu options have and have not yet been chosen.  
+ * <br>- All data members must have values that follow the rules specified by the {@link #validateMembers() validateMembers} method.
+ * <br>- All constructors validate given values. If creating a degenerate Menu is necessary, build it using mutators.
  * @author Michael Bradley
  *
  */
@@ -33,14 +31,13 @@ public class Menu {
 	public static final String[] ON_OFF = {"On", "Off"};
 	public static final String[] YES_NO = {"Yes", "No"};
 
-	private static Scanner input = new Scanner(System.in);
+	protected static Scanner input = new Scanner(System.in);
 
 	protected int choice;
 	protected int lastChoice;
 
 	protected int exitOn;
 	protected boolean exitAllowed;
-//	protected boolean incompleteExit;
 
 	protected int numOptions;
 	protected String[] options;
@@ -97,7 +94,7 @@ public class Menu {
 		setOptions(options);
 
 		setTitle(title);
-		this.exitText = exitText;
+		setExitText(exitText);
 
 		initChosen();
 		this.chosenText = chosenText;
@@ -109,7 +106,6 @@ public class Menu {
 	/**
 	 * Sets all properties necessary to create a complete {@link Menu}.
 	 * @param title
-	 * @param numOptions
 	 * @param options
 	 * @param exitAllowed
 	 * @param exitOn
@@ -124,7 +120,7 @@ public class Menu {
 		this.exitAllowed = exitAllowed;
 		this.exitOn = exitOn;
 
-		numOptions = options.length;
+		setNumOptions(options);
 		setOptions(options);
 
 		setTitle(title);
@@ -178,7 +174,6 @@ public class Menu {
 	 * Sets all properties necessary to create an unexitable, choice-tracking {@link Menu}.
 	 * @param title
 	 * @param options
-	 * @param incompleteExit
 	 * @param chosenText
 	 * @param unchosenText
 	 */
@@ -215,6 +210,14 @@ public class Menu {
 		this(title, options.length, options);
 	}
 
+	/**
+	 * Uses all of the values from another Menu object to create a new one. Validates after copying.
+	 * @param other
+	 */
+	public Menu(Menu other) {
+		copy(other);
+		validateMembers();
+	}
 
 
 	@Override
@@ -245,7 +248,7 @@ public class Menu {
 	 * Gets the menu choice from the standard input.
 	 * <br>Prompts for input as long as the value is either out of range or invalid.
 	 * <br>Does not change the chosen booleans.
-	 * @return
+	 * @return a reference to this Menu object
 	 */
 	public Menu getInput() {
 		return getInput(false);
@@ -268,7 +271,7 @@ public class Menu {
 			} catch (InputMismatchException e) {
 				input.next();
 			}
-		} while ((!exitAllowed || c != exitOn) && (c < 1 || c > numOptions));
+		} while (!isValidOption(c));
 
 		lastChoice = choice;
 		choice = c;
@@ -280,10 +283,14 @@ public class Menu {
 		return this;
 	}
 
+	/**
+	 * @param showChosen if true outputs choice tracking text and leaves them out otherwise.
+	 * @return The menu as a String, including all title, all options, choice tracking and exit option if applicable.
+	 */
 	public String toString(boolean showChosen) {
 		StringBuilder ret = new StringBuilder(title);
 
-		//Add a line under the title that is the length of the title
+		//Add a line the length of the title underneath the title 
 		if (!title.equals("")) {
 			ret.append("\n" + new String(new char[title.length()]).replace('\0', '-') + "\n");
 		}
@@ -305,22 +312,50 @@ public class Menu {
 		return ret.toString();
 	}
 
+	/**
+	 * This method does the same thing as {@link #toString(boolean) toString(false)}
+	 * @return The menu as a String, including all title, all options and exit option if applicable. 
+	 */
 	@Override
 	public String toString() {
 		return toString(false);
 	}
 
-	//Don't forget to write this and the copy constructor!
+	/**
+	 * Copies all of another {@link Menu}'s values into this.
+	 * Does not {@link #validateMembers() validate} the values.
+	 * @param other
+	 */
 	public void copy(Menu other) {
-
+		setChoice(other.getChoice());
+		setLastChoice(other.getChoice());
+		
+		setExitOn(other.getExitOn());
+		setExitAllowed(other.isExitAllowed());
+		
+		setNumOptions(other.getNumOptions());
+		setOptions(other.getOptions());
+		
+		setTitle(other.getTitle());
+		setExitText(other.getExitText());
+		
+		setChosen(other.getChosen());
+		setChosenText(other.getChosenText());
+		setUnchosenText(other.getUnchosenText());
 	}
 
 
 	//-- Convenience --\\
+	/**
+	 * @return whether the most recent choice was the exit option.
+	 */
 	public boolean didChooseExit() {
 		return choice == exitOn;
 	}
 
+	/**
+	 * @return whether the most recent choice was the same as the previous choice.
+	 */
 	public boolean didChooseSame() {
 		return choice == lastChoice;
 	}
@@ -329,7 +364,7 @@ public class Menu {
 	/**
 	 * Validates data members according to these rules:
 	 * <br>- numOptions cannot be less than 0
-	 * <br>- exitOn cannot be the same number as one of the choices
+	 * <br>- exitOn cannot be the same number as one of the options
 	 * <br>- length of options array must be equal to numChoices
 	 * <br>- length of chosen array must be equal to numChoices
 	 * <br>- numOptions cannot be 0 if exiting is not allowed
@@ -356,27 +391,55 @@ public class Menu {
 			throw new InvalidMemberException("The number of options is zero and exiting is not allowed.");
 		}
 	}
+	
+	/**
+	 * Checks whether the argument is a valid menu option. 
+	 * @param choice
+	 * @return true if choice is equal to one of the options or if exiting is allowed and choice is equal to the exit option, otherwise false.
+	 */
+	public boolean isValidOption(int choice) {
+		return ((!exitAllowed || choice != exitOn) && (choice < 1 || choice > numOptions)) ? false : true;
+	}
 
 
 	//-- Accessors and Mutators --\\
+	/**
+	 * @return the most recent choice.
+	 */
 	public int getChoice() {
 		return choice;
 	}
 
+	/**
+	 * Sets the most recent choice.
+	 * @param option
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setChoice(int option) {
 		choice = option;
 		return this;
 	}
 
+	/**
+	 * @return the previous choice.
+	 */
 	public int getLastChoice() {
 		return lastChoice;
 	}
 
+	/**
+	 * Sets the previous choice.
+	 * @param option
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setLastChoice(int option) {
 		lastChoice = option;
 		return this;
 	}
 
+	/**
+	 * @return the number of options excluding the exit option.
+	 */
 	public int getNumOptions() {
 		return numOptions;
 	}
@@ -385,64 +448,110 @@ public class Menu {
 	 * Sets the number of options excluding the exit option.
 	 * Must be greater than or equal to zero
 	 * @param choices
-	 * @return a reference to this {@link Menu} object
+	 * @return a reference to this {@link Menu} object.
 	 */
 	public Menu setNumOptions(int choices) {
 		numOptions = choices;
 		return this;
 	}
+	
+	/**
+	 * Sets the number of options equal to the length of the array or zero if a null argument is received.
+	 * @param options
+	 * @return a reference to this Menu object.
+	 */
+	public Menu setNumOptions(String[] options) {
+		if (options == null) {
+			numOptions = 0;
+		} else {
+			numOptions = options.length;
+		}
+		
+		return this;
+	}
 
+	/**
+	 * @return whether exiting this Menu with the exit option is possible.
+	 */
 	public boolean isExitAllowed() {
 		return exitAllowed;
 	}
 
+	/**
+	 * Sets whether exiting this Menu with the exit option is possible.
+	 * @param allowed
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setExitAllowed(boolean allowed) {
 		exitAllowed = allowed;
 		return this;
 	}
 
+	/**
+	 * @return the exit option's number.
+	 */
 	public int getExitOn() {
 		return exitOn;
 	}
 
 	/**
-	 * Option number to exit
+	 * Sets the option number to exit on.
 	 * <br>Must not be in the range [1, numOptions]
 	 * @param option
-	 * @return a reference to this {@link Menu} object
+	 * @return a reference to this {@link Menu} object.
 	 */
 	public Menu setExitOn(int option) {
 		exitOn = option;
 		return this;
 	}
 
+	/**
+	 * @return the title of the Menu or an empty string if no title was set.
+	 */
 	public String getTitle() {
 		return title;
 	}
 
+	/**
+	 * Sets the title of the Menu. Passing a null reference to this method will set the title to an empty String.
+	 * @param text
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setTitle(String text) {
 		title = text == null ? "" : text;
 		return this;
 	}
 
+	/**
+	 * @return the exit option's text.
+	 */
 	public String getExitText() {
 		return exitText;
 	}
 
+	/**
+	 * Sets the Menu's exit option's text. Passing a null reference to this method will set the exit text to an empty String.
+	 * @param text
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setExitText(String text) {
-		exitText = text;
+		exitText = text == null ? "" : text;
 		return this;
 	}
 
+	/**
+	 * @return the options as an array of Strings 
+	 */
 	public String[] getOptions() {
 		return options;
 	}
 
 	/**
 	 * Copies the passed String array into a new one of the same size.
-	 * <br>Can also take a null argument to create an empty String array the size of numOptions.
+	 * <br>A null argument will create an empty String array of size numOptions.
+	 * <br>Does not change numOptions.
 	 * @param choices
-	 * @return
+	 * @return a reference to this Menu object.
 	 */
 	public Menu setOptions(String[] choices) {
 		if (choices == null) {
@@ -457,27 +566,40 @@ public class Menu {
 		}
 		return this;
 	}
-	//Should there be another method that allows this object's options array to be a reference to the argument?
-	//Same idea for chosen array
 
+	/**
+	 * Note: index will be one less than the displayed option number.
+	 * @param index
+	 * @return the option at the specified index.
+	 */
 	public String getOptionAt(int index) {
 		return options[index];
 	}
 
+	/**
+	 * Sets the option at the specified index with a copy of the passed String.
+	 * <br>Note: index will be one less than the displayed option number.
+	 * @param index
+	 * @param option
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setOptionAt(int index, String option) {
 		options[index] = new String(option);
 		return this;
 	}
 
+	/**
+	 * @return the choice tracking array of booleans.
+	 */
 	public boolean[] getChosen() {
 		return chosen;
 	}
 
 	/**
-	 * Copies the passed String array into a new one of the same size.
-	 * <br>Can also take a null argument to create an empty boolean array the size of numOptions.
-	 * @param choices
-	 * @return
+	 * Copies the passed boolean array into a new one of the same size.
+	 * <br>A null argument will create an empty boolean array of size numOptions.
+	 * @param chosen
+	 * @return a reference to this Menu object.
 	 */
 	public Menu setChosen(boolean[] chosen) {
 		if (chosen == null) {
@@ -495,8 +617,7 @@ public class Menu {
 
 	/**
 	 * Allocates a new boolean array of size numOptions and sets all elements to false.
-	 * <br>Be sure to call, if resetting the options.
-	 * @return
+	 * @return a reference to this Menu object.
 	 */
 	public Menu initChosen() {
 		chosen = new boolean[numOptions];
@@ -505,15 +626,31 @@ public class Menu {
 		return this;
 	}
 
+	/**
+	 * Note: index will be one less than the displayed option number.
+	 * @param index
+	 * @return whether the specified option has been chosen.
+	 */
 	public boolean isChosenAt(int index) {
 		return chosen[index];
 	}
 
+	/**
+	 * Sets whether the options has been chosen at the specified index.
+	 * <br>Note: index will be one less than the displayed option number.
+	 * @param index
+	 * @param picked
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setChosenAt(int index, boolean picked) {
 		chosen[index] = picked;
 		return this;
 	}
 
+	/**
+	 * Convenience method
+	 * @return whether all options have been chosen at least once.
+	 */
 	public boolean isComplete() {
 		for (int i = 0; i < numOptions; i++) {
 			if (!chosen[i]) {
@@ -524,12 +661,21 @@ public class Menu {
 		return true;
 	}
 
-	public void setComplete(boolean complete) {
+	/**
+	 * Sets all of the elements in the choice tracking array to the passed value.
+	 * @param complete
+	 * @return a reference to this Menu object.
+	 */
+	public Menu setComplete(boolean complete) {
 		for (int i = 0; i < numOptions; i++) {
 			chosen[i] = complete;
 		}
+		return this;
 	}
 
+	/**
+	 * @return the number of options that have been chosen.
+	 */
 	public int getNumChosen() {
 		int ret = 0;
 
@@ -542,25 +688,44 @@ public class Menu {
 		return ret;
 	}
 
+	/**
+	 * @return the number of options that have not been chosen.
+	 */
 	public int getNumUnchosen() {
 		return numOptions - getNumChosen();
 	}
 
+	/**
+	 * @return the text displayed next to options that have been chosen.
+	 */
 	public String getChosenText() {
 		return chosenText;
 	}
 
+	/**
+	 * Sets the text displayed next to options that have been chosen. Passing a null reference to this method will set the chosen text to an empty String.
+	 * @param text
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setChosenText(String text) {
-		chosenText = text;
+		chosenText = text == null ? "" : text;
 		return this;
 	}
 
+	/**
+	 * @return the text displayed next to options that have not been chosen.
+	 */
 	public String getUnchosenText() {
 		return unchosenText;
 	}
 
+	/**
+	 * Sets the text displayed next to options that have not been chosen. Passing a null reference to this method will set the unchosen text to an empty String.
+	 * @param text
+	 * @return a reference to this Menu object.
+	 */
 	public Menu setUnchosenText(String text) {
-		unchosenText = text;
+		unchosenText = text == null ? "" : text;
 		return this;
 	}
 
@@ -568,12 +733,12 @@ public class Menu {
 
 	//-- Building and Saving Menus --\\
 	/**
-	 * Builds a {@link Menu} object using JSON. 
+	 * Builds a {@link Menu} object from a JSON file. 
 	 * @param path
-	 * @return the {@link Menu} object or null if something went wrong
+	 * @return the {@link Menu} object or null if the Menu could not be built
 	 * @throws FileNotFoundException
 	 */
-	public static Menu buildFromFile(String path) throws FileNotFoundException  {
+	public static Menu loadFromFile(String path) throws FileNotFoundException  {
 		Menu ret = null;
 
 		try (JsonReader jr = Json.createReader(new FileInputStream(path))) {
@@ -601,7 +766,12 @@ public class Menu {
 		return ret;
 	}
 
-	
+	/**
+	 * Writes a {@link Menu} to a JSON file.
+	 * @param path
+	 * @param menu
+	 * @throws FileNotFoundException
+	 */
 	public static void saveToFile(String path, Menu menu) throws FileNotFoundException {
 		menu.validateMembers();
 		
@@ -625,6 +795,13 @@ public class Menu {
 		}
 	}
 
+	/**
+	 * Convenience method that handles adding a null String to the JsonObjectBuilder. 
+	 * @param key the String object in the Menu
+	 * @param value the text to be displayed
+	 * @param obj
+	 * @return a reference to the passed {@link JsonObjectBuilder}
+	 */
 	private static JsonObjectBuilder addString(String key, String value, JsonObjectBuilder obj) {
 		if (value == null) {
 			obj.add(key, JsonValue.NULL);
