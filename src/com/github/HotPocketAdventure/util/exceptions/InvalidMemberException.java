88f57bb76285913faa03a7784241a2b056fa669f
Michael Bradley <mbrad94@gmail.com>
2017-02-13 08:39:15 -0800

    Documentation edits
    - documentation now references validation rules

--- src/com/github/HotPocketAdventure/util/exceptions/InvalidMemberException.java
+++ src/com/github/HotPocketAdventure/util/exceptions/InvalidMemberException.java
@@ -1,10 +1,13 @@
 package com.github.HotPocketAdventure.util.exceptions;
 
+/**
+ * An exception to log any error caused by degenerate Menus.
+ * <br>Reference the {@link com.github.HotPocketAdventure.util.Menu Menu}'s rules {@link com.github.HotPocketAdventure.util.Menu#validateMembers() here}.
+ * @author Michael Bradley
+ *
+ */
 public class InvalidMemberException extends RuntimeException {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 0xDEAD;
 
 	public InvalidMemberException() {
